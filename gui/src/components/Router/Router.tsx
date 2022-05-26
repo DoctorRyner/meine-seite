d6013373b9833b0726f03aaa269f37ccacef0a01
DoctorRyner <drryner@icloud.com>
2022-05-27 02:31:16 +0400

    Remove the page function and deplace it with the Page enum

--- gui/src/components/Router/Router.tsx
+++ gui/src/components/Router/Router.tsx
@@ -1,20 +1,20 @@
 import { App } from "@/components/App/App"
 import { Home } from "@/pages/Home/Home"
 import { Test } from "@/pages/Test/Test"
-import { identity } from "ramda"
 import { FC } from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
-export type Page = "/" | "test"
-
-export const page: (path: Page) => Page = identity
+export enum Page {
+  home = "/",
+  test = "test"
+}
 
 export const Router: FC = () => (
   <BrowserRouter>
     <Routes>
-      <Route path={page("/")} element={<App />}>
+      <Route path={Page.home} element={<App />}>
         <Route index element={<Home />} />
-        <Route path={page("test")} element={<Test />} />
+        <Route path={Page.test} element={<Test />} />
       </Route>
     </Routes>
   </BrowserRouter>

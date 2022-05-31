2fd4b094de77c0154f53fd73459694d88ce93e3f
DoctorRyner <drryner@icloud.com>
2022-06-01 00:26:50 +0400

    Add the first landing block (WIP)

--- gui/src/pages/Portfolio/PortfolioRegiM/PortfolioRegiM.tsx
+++ gui/src/pages/Portfolio/PortfolioRegiM/PortfolioRegiM.tsx
@@ -5,9 +5,25 @@ import { FC } from "react"
 
 export const PortfolioRegiM: FC = () => (
   <div className={styles.page}>
+    <div className={styles.elipse} />
     <nav className={styles.navbar}>
       <img src={logo} className={styles.navbarLogo} />
       <RegiMButton>Get Early Access</RegiMButton>
     </nav>
+    <div className={styles.landingBlock1}>
+      <div className={styles.landingBlock1TitleContainer}>
+        <h1 className={styles.landingBlock1TitleContainerTitle}>
+          Event Registration Reimagined
+        </h1>
+        <p className={styles.landingBlock1TitleContainerParagraph}>
+          Manage your event registrations hassle free and without any dependency
+          with the technical team. Isn’t it sounds amazing?
+        </p>
+        <div className={styles.landingBlock1TitleContainerButton}>
+          <RegiMButton type="big">Get Early Access</RegiMButton>
+        </div>
+      </div>
+      <div></div>
+    </div>
   </div>
 )
